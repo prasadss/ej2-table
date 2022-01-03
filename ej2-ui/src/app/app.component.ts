3b37499e7b044ec2a44bda7f631c3029639e46a6
psaitwal <prasad.saitwal@finicity.com>
2022-01-03 13:09:23 +0530

    Column chooser added

--- ej2-ui/src/app/app.component.ts
+++ ej2-ui/src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { VirtualScrollService, TreeGridComponent } from '@syncfusion/ej2-angular-treegrid';
+import {
+  VirtualScrollService,
+  TreeGridComponent,
+  ColumnChooserService,
+  ToolbarService,
+} from '@syncfusion/ej2-angular-treegrid';
 import { dataSource, virtualData } from './datasource';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [VirtualScrollService]
+  providers: [VirtualScrollService, ColumnChooserService, ToolbarService],
 })
 export class AppComponent {
   title = 'ej2-ui';
-  data: Object[];
+  public data: Object[];
+  public toolbar: string[];
 
   ngOnInit(): void {
-      dataSource();
-      this.data = virtualData;
+    dataSource();
+    this.data = virtualData;
+    this.toolbar = ['ColumnChooser'];
   }
 }
