385d1b9cbc12e5b367aca0cb40e29c8649e26166
prasadss <prasadssaitwal@gmail.com>
2022-01-03 22:42:41 +0530

    Added commands for heroku

--- ej2-ui/server.js
+++ ej2-ui/server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/ej2-ui'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/ej2-ui/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/ej2-ui/index.html'));});
 app.listen(process.env.PORT || 8080);
